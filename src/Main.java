9d5b6fd19a23eef570b109b95079e200b5743202
Santiago Martino <santiago.martino.eu@gmail.com>
2026-07-04 17:54:25 -0300

    Refatorado o código do arquivo Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
-    public static void main(String[] args) {
+    private static void sayHello() {
         System.out.println("Olá mundo!");
+    }
+    public static void main(String[] args) {
+        sayHello();
     }
 
 }
